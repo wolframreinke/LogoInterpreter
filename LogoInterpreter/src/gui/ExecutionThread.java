cdf772db268594ad4aafc93e4f850a03f362bf1d
Julian Schäfer <julian-schaefer@live.de>
2014-07-06 16:33:22 +0200

    added ExecutionThred class

---
+++ LogoInterpreter/src/gui/ExecutionThread.java
@@ -0,0 +1,28 @@
+package gui;
+
+import gui.elements.ErrorMessanger;
+import gui.elements.RunButton;
+import gui.elements.SourceCodeEditorPane;
+import gui.elements.StatusOutput;
+
+
+public class ExecutionThread extends Thread {
+
+	private boolean isRunning = false;
+	
+	public ExecutionThread(RunButton runButton, SourceCodeEditorPane sourceCodeEditorPane, DrawTurtle drawTurtle, StatusOutput statusOutput, ErrorMessanger errorMessanger){
+		
+	}
+	
+	public void toggle(){
+		
+	}
+	
+	public void reset(){
+		
+	}
+	
+	public void step(){
+		
+	}
+}
