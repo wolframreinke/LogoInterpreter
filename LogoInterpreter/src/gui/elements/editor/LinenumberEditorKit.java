f060218b579d7f3c1a215579be073f0da4952ced
Wolfram Reinke <WolframReinke@web.de>
2014-07-13 14:31:18 +0200

    created huge UML diagram

--- LogoInterpreter/src/gui/elements/editor/LinenumberEditorKit.java
+++ LogoInterpreter/src/gui/elements/editor/LinenumberEditorKit.java
@@ -11,50 +11,102 @@ import javax.swing.text.StyledEditorKit;
 import javax.swing.text.View;
 import javax.swing.text.ViewFactory;
 
-
+/**
+ * A <code>LinenumberEditorKit</code> is a <code>StyledEditorKit</code> which addes
+ * a line number bar on the left hand side of <code>SourceCodeEditorPane</code>s.
+ * 
+ * @author Wolfram Reinke
+ * @version 1.0
+ * 
+ */
 @SuppressWarnings( "serial" )
 public class LinenumberEditorKit extends StyledEditorKit {
 
+	/**
+	 * A <code>LinenumberViewFactory</code> creates views of some portions of a
+	 * <code>SourceCodeEditorPane</code>'s document. Among others, it addes a line
+	 * number bar on the left hand side of the <code>SourceCodeEditorPane</code>. 
+	 * 
+	 * @author Wolfram Reinke
+	 * @version 1.0
+	 * @see {@link LinenumberParagraphView}
+	 */
 	public static class LinenumberViewFactory implements ViewFactory {
 
+		/**
+		 * The line number bar will be added to this <code>SourceCodeEditorPane</code>.
+		 */
 		private SourceCodeEditorPane editor;
 
+		/**
+		 * Creates a new <code>LinenumberViewFactory</code> which creates views for
+		 * the specified <code>SourceCodeEditorPane</code>.
+		 *
+		 * @param editor
+		 * 		The <code>SourceCodeEditorPane</code> for which the views are
+		 * 		created. If this value is <code>null</code>, the 
+		 * 		current-line-highlighting will not work, but line numbers will be added
+		 * 		though.
+		 */
 		public LinenumberViewFactory( SourceCodeEditorPane editor ) {
+
 			this.editor = editor;
 		}
-		
+
+		/**
+		 * Creates a suitable view for the given element.
+		 * 
+		 * @param element
+		 * 		The <code>Element</code>, for which the view is created.
+		 * 
+		 * @return
+		 * 		A suitable view for the specified element.
+		 * 
+		 */
 		@Override
 		public View create( Element element ) {
 
 			String type = element.getName();
-			
+
 			switch ( type ) {
-			
+
 			case AbstractDocument.ContentElementName:
 				return new LabelView( element );
-				
+
 			case AbstractDocument.ParagraphElementName:
-				return new LinenumberParagraphView( element, this.editor );	
-				
+				return new LinenumberParagraphView( element, this.editor );
+
 			case AbstractDocument.SectionElementName:
 				return new BoxView( element, View.Y_AXIS );
-				
+
 			case StyleConstants.ComponentElementName:
 				return new ComponentView( element );
-				
+
 			case StyleConstants.IconElementName:
 				return new IconView( element );
-				
+
 			default:
 				return new LabelView( element );
-				
+
 			}
 		}
-		
+
 	}
 
+	/**
+	 * The line number bar will be added to this <code>SourceCodeEditorPane</code>.
+	 */
 	private SourceCodeEditorPane editor;
-	
+
+	/**
+	 * Creates a new <code>LinenumberEditorKit</code> with the specified
+	 * <code>SourceCodeEditorPane</code>.
+	 *
+	 * @param editor
+	 * 		The <code>SourceCodeEditorPane</code> to which the line number bar will
+	 * 		be added. If this parameter is <code>null</code>, the line number bar
+	 * 		will be added though, but the current-line-highlighting won't work.
+	 */
 	public LinenumberEditorKit( SourceCodeEditorPane editor ) {
 
 		super();
@@ -63,8 +115,7 @@ public class LinenumberEditorKit extends StyledEditorKit {
 
 	@Override
 	public ViewFactory getViewFactory() {
-		
+
 		return new LinenumberViewFactory( this.editor );
 	}
 }
-
--- LogoInterpreter/src/gui/elements/editor/LinenumberParagraphView.java
+++ LogoInterpreter/src/gui/elements/editor/LinenumberParagraphView.java
@@ -8,12 +8,40 @@ import javax.swing.text.Element;
 import javax.swing.text.ParagraphView;
 import javax.swing.text.View;
 
+/**
+ * The <code>LinenumberParagraphView</code> displays the line number on the left
+ * hand side of each line.
+ * 
+ * @author Wolfram Reinke
+ * @version 1.0
+ *
+ */
 public class LinenumberParagraphView extends ParagraphView {
 
+	/**
+	 * The width in pixels of the line number bar.
+	 */
 	private static final short NUMBERS_WIDTH = 30;
 
+	/**
+	 * The <code>SourceCodeEditorPane</code> which is used to retrieve the current
+	 * line. This line won't be displayed in light gray, but in blue.
+	 */
 	private SourceCodeEditorPane editor;
 	
+	/**
+	 * Creates a new <code>LinenumberParagraphView</code> of the given element. The
+	 * specified <code>SourceCodeEditorPane</code> is used to highlight the current
+	 * line number.
+	 *
+	 * @param element
+	 * 		The element for which this <code>View</code> is created.
+	 * 
+	 * @param editor
+	 * 		The editor whose current line number will be rendered specially in the
+	 * 		line number bar.
+	 * 
+	 */
 	public LinenumberParagraphView( Element element, SourceCodeEditorPane editor ) {
 
 		super( element );
@@ -27,31 +55,42 @@ public class LinenumberParagraphView extends ParagraphView {
 		this.setInsets( top, left, bottom, right );
 	}
 
+	/*
+	 * overwritten Javadoc comment is suitable
+	 */
 	@Override
 	protected void setInsets( short top, short left, short bottom, short right ) {
 
 		super.setInsets( top, (short) (left + NUMBERS_WIDTH), bottom, right );
 	}
 
+	/**
+	 * Paints the line number bar on the left hand side by using the specified
+	 * <code>Graphics</code> object.
+	 * 
+	 */
 	@Override
 	protected void paintChild( Graphics g, Rectangle alloc, int index ) {
 
 		int lineNumber = getPreviosLineCount() + index + 1;
 		int numberX = alloc.x - getLeftInset();
+		// the 3 is used to move the output to the center of the line
 		int numberY = alloc.y + alloc.height - 3;
 
 		
 		Color c;
-		if ( lineNumber == this.editor.getCurrentLineNumber() )
+		if ( this.editor != null && lineNumber == this.editor.getCurrentLineNumber() )
 			c = Color.BLUE;
 		else
-			c = new Color( 0xF9, 0xF9, 0xF9 );
+			c = new Color( 0xF9, 0xF9, 0xF9 ); // very light gray
 		
 		g.setColor( c );
 
+		// fill the bar with the color
 		g.fillRect( alloc.x - getLeftInset(), alloc.y, getLeftInset() - 5,
 				alloc.height );
 
+		// and apint the number on it
 		g.setColor( Color.BLACK );
 		g.drawString( Integer.toString( lineNumber ), numberX, numberY );
 
@@ -59,6 +98,12 @@ public class LinenumberParagraphView extends ParagraphView {
 
 	}
 
+	/**
+	 * Calculates the number of lines before this <code>View</code>.
+	 *
+	 * @return
+	 * 		The number of lines before this view.
+	 */
 	private int getPreviosLineCount() {
 
 		int lineCount = 0;
